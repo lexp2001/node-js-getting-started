2bd7659bc7c1fbd2116f069c6ea313a4b124d61e
Leonardo Barreto <leonardojbarreto@gmail.com>
2021-02-28 10:05:52 -0300

    Get all workers

--- index.js
+++ index.js
@@ -29,5 +29,18 @@ express()
       res.send("Error " + err);
     }
   })
+  .get('/workers', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM rep_workers');
+      const results = { 'results': (result) ? result.rows : null};
+      //res.render('pages/db', results );
+      res.json(results);
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
   .get('/cool', (req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
