ac3243c4294801eb78972c6f1c7a1b5cf326cb0d
Leonardo Barreto <leonardojbarreto@gmail.com>
2021-02-28 14:54:33 -0300

    Create a worker

--- index.js
+++ index.js
@@ -79,17 +79,17 @@ express()
     try {
       // console.info(req.body);
       // console.info(req);
-      // const client = await pool.connect();
-      // const result = await client.query(`INSERT INTO rep_workers values ('${req.body.id}', '${req.body.name}', '${req.body.address}', '${req.body.category}', ${req.body.score}, '${req.body.cover_image}', ${req.body.lat}, ${req.body.long}, '${req.body.description}'`);
-      // const results = { 'results': (result) ? result.rows : null};
-      //res.render('pages/db', results );
-      //res.json(results);
-      res.json({"reqBody": req.body});
-      //client.release();
+      const client = await pool.connect();
+      const result = await client.query(`INSERT INTO rep_workers values ('${req.body.id}', '${req.body.name}', '${req.body.address}', '${req.body.category}', ${req.body.score}, '${req.body.cover_image}', ${req.body.lat}, ${req.body.long}, '${req.body.description}'`);
+      const results = { 'results': (result) ? result.rows : null};
+
+      res.json(results);
+      //res.json({"reqBody": req.body});
+      client.release();
     } catch (err) {
       // console.error(err);
-      //res.send("Error " + err);
-      res.json({"Error ": err, "reqBody": req.body, "req": req});
+      res.send("Error " + err);
+      //res.json({"Error ": err, "reqBody": req.body, "req": req});
     }
   })
   .get('/cool', (req, res) => res.send(cool()))
